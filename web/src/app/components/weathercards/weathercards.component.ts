61594db8f81264188839824fa1fb857f9fb4c6d7
BenjiG70 <markus.tabler@tutanota.com>
2024-09-30 21:14:37 +0200

    card changes + database server

--- web/src/app/components/weathercards/weathercards.component.ts
+++ web/src/app/components/weathercards/weathercards.component.ts
@@ -1,3 +1,4 @@
+import { TypeModifier } from '@angular/compiler';
 import { Component, Input } from '@angular/core';
 
 @Component({
@@ -11,16 +12,46 @@ export class WeathercardsComponent {
   formattedDate: string = ''; // Für das formatierte Datum
 
   @Input() sensor:string = "default";
-  @Input() temperature:number = 0;
-  @Input() humidity:number = 0;
-  @Input() air_pressure:number=0;
+  private _temperature: number = 0;
+  private _humidity: number = 0;
+  private _airPressure: number = 0;
+
+  @Input()
+  set temperature(value: number) {
+    this._temperature = this.round(value);
+  }
+  
+  get temperature(): number {
+    return this._temperature;
+  }
+
+  @Input()
+  set humidity(value: number) {
+    this._humidity = this.round(value);
+  }
+  
+  get humidity(): number {
+    return this._humidity;
+  }
+
+  @Input()
+  set air_pressure(value: number) {
+    this._airPressure = this.round(value);
+  }
+  
+  get air_pressure(): number {
+    return this._airPressure;
+  }
+
+  // Rundungsfunktion auf eine Dezimalstelle
+  private round(value: number): number {
+    return Math.round(value * 10) / 10; // Rundet auf eine Dezimalstelle
+  }
   @Input() rain:boolean = false;
   @Input()
-
   set date(value: any) {
     // Stelle sicher, dass der Wert ein Zeitstempel ist
     this._date = new Date(Number(value)); // Umwandlung in ein Date-Objekt
-    console.log('Eingangswert für date:', value); // Debugging-Ausgabe
     this.formattedDate = this.formatDate(this._date); // Formatiere das Datum
   }
 
